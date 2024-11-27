a5366016bad855a42f77dc3c86204da2c5bb5f4e
ChuN6868 <kanaircla@gmail.com>
2024-11-27 09:23:36 +0900

    feat/try-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Cocktify',
+        short_name: 'Cocktify',
+        description: 'Cocktail App built with React and Vite',
+        theme_color: '#181F27',
+        background_color: '#181F27',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ],
+  // server: {
+  //   https: true
+  // }
 })
 
 
